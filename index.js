02ee90572f52e38796a33a60348bb9ae07fcef67
kenneth o cruz <cruzkenneth504@gmail.com>
2023-03-30 02:02:59 -0700

    add if statement

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const upperCase = "ABCDEFGHJKPQRSTUXYZ";
 const numbers = "0123456789"
 const speaciaCharcters = "~`!@#$%^&*()-_+={}[]|\;:<>,./?";
 const pwLenght ="";
-  
-function generatePassword(){
 
-}
 
 // Write password to the #password input
 function writePassword() {
@@ -18,6 +15,26 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  userPassword = "";
+  passwordCharacters = ""
+
+  pwLenght = prompt(
+    "Select password length. (8-10 Charachters)")
+  ;
+ 
+  if (pwLenght >= 8 && pwLenght <= 20){
+    password.lenght = pwLenght
+ 
+  var isLow = confirm("Include lowercase letters in password ");
+  var isUpp = confirm("Include uppercase letters in password");
+  var isNum = confirm("Include numbers in password")
+  var Chartset = confirm("Include special characters in password")
+}else{
+  alert("Please Select a password between 8-20 characters")
+}
 }
 
 // Add event listener to generate button
